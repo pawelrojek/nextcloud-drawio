230316ad8dc1af2bd5845b6169e3f969c9c1b75f
Phil's Lab <philmolitor.molitor@googlemail.com>
2018-03-15 18:06:16 +0100

    fix filePath when containing special characters

--- drawio/templates/editor.php
+++ drawio/templates/editor.php
@@ -18,7 +18,7 @@
 				OCA.DrawIO.DisplayError("<?php p($_['error']) ?>");
 			<?php } else { ?>
 				var iframe = $("#iframeEditor")[0];
-				var filePath = "<?php p($_['filePath']); ?>";
+				var filePath = "<?php echo urldecode($_['filePath']); ?>";
 				var originUrl = "<?php p($_['drawioUrl']); ?>";
 				var drawIoUrl = "<?php p($_['drawioUrl']); print_unescaped($frame_params); ?>"
 				OCA.DrawIO.EditFile(iframe.contentWindow, filePath, originUrl);
